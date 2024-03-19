41d764d0897a7880355c3801021eb68ce15c7a70
Steven Wilkin <stevenwilkin@gmail.com>
2024-03-19 14:40:19 +0000

    Init and access database within activity lifecycle

--- app/src/main/java/com/example/justfortoday/MainActivity.kt
+++ app/src/main/java/com/example/justfortoday/MainActivity.kt
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.room.Room
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var entries: EntryDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,13 +24,28 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val db = Room
-                    .databaseBuilder(applicationContext, EntryDatabase::class.java, "jft")
-                    .createFromAsset("nevercured.db")
-                    .allowMainThreadQueries()
-                    .build()
+        initDB()
+    }
 
-        val entry = db.entryDao().getAll().first()
-        Log.d("jft", entry.summary.toString())
+    override fun onResume() {
+        super.onResume()
+        displayCurrentEntry()
+    }
+
+    private fun initDB() {
+        val db = Room
+            .databaseBuilder(applicationContext, EntryDatabase::class.java, "jft")
+            .createFromAsset("nevercured.db")
+            .build()
+
+        entries = db.entryDao()
+    }
+
+    private fun displayCurrentEntry() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val entry = entries.getAll().first()
+            Log.d("jft", entry.summary.toString())
+        }
+
     }
 }
